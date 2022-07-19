203176adc25de59a231fda58f8d80e5865eabbbc
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-07-19 15:39:16 -0300

    V1.4.2(Card-Components/fix)

---
+++ src/components/CopyButtonCard.jsx
@@ -0,0 +1,58 @@
+import {
+  Box,
+  useDisclosure,
+  Button,
+  Collapse,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+export default function CopyButtonCard({ props }) {
+  const { isOpen, onToggle } = useDisclosure();
+  return (
+    <Stack width="100%">
+      <Button onClick={onToggle}>Ingredients</Button>
+      <Collapse in={isOpen} animateOpacity>
+        <Stack
+          direction="row"
+          justify="space-between"
+          gap={{ base: "10px", md: "15px", lg: "25px" }}
+          p="40px"
+          color="white"
+          bg="teal.500"
+          rounded="md"
+          shadow="md"
+          width="100%"
+        >
+          <Box>
+            {props.ingredients?.map((e, index) => {
+              if (e === "" || e === " ") {
+                return;
+              } else {
+                return (
+                  <Text key={index} textTransform="capitalize" fontSize="md">
+                    {e}
+                  </Text>
+                );
+              }
+            })}
+          </Box>
+
+          <Box>
+            {props.measure?.map((e, index) => {
+              if (e === "" || e === " ") {
+                return;
+              } else {
+                return (
+                  <Text key={index} textTransform="-moz-initial" fontSize="md">
+                    {e}
+                  </Text>
+                );
+              }
+            })}
+          </Box>
+        </Stack>
+      </Collapse>
+    </Stack>
+  );
+}
